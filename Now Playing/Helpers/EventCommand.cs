a5ccfe0126a6098b69c77adbb1502ec2c3859e7e
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-06-19 12:51:20 -0700

    Add EventCommand.cs

---
+++ Now Playing/Helpers/EventCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Now_Playing.Helpers;
+
+public class EventCommand : ICommand
+{
+    public event EventCommandExecutedEventHandler Executed;
+
+    public void Execute(object parameter) => Executed?.Invoke(this, parameter);
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter) => true;
+}
+
+public delegate void EventCommandExecutedEventHandler(EventCommand sender, object args);
