909637b36a69b273df28acf197a86d1d3dc5bb59
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-06-19 12:52:53 -0700

    Add SettingsWindow.xaml

---
+++ Now Playing/Options/SettingsWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Now_Playing.Options
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
