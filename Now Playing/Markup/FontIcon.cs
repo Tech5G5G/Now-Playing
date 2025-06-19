ac498efb8ea345a4f0fc06c0cb35d8ef4ef9422a
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-06-19 12:50:39 -0700

    Add FontIcon markup extension

---
+++ Now Playing/Markup/FontIcon.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Markup;
+
+namespace Now_Playing.Markup
+{
+    public class FontIcon : MarkupExtension
+    {
+        public string Glyph { get; set; }
+
+        public double FontSize { get; set; } = 12;
+
+        public FontFamily FontFamily { get; set; } = SystemFonts.MessageFontFamily;
+
+        public FontWeight FontWeight { get; set; } = FontWeights.Normal;
+
+        public FontStyle FontStyle { get; set; } = FontStyles.Normal;
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => new TextBlock
+        {
+            Text = Glyph,
+            FontSize = FontSize,
+            FontFamily = FontFamily,
+            FontWeight = FontWeight,
+            FontStyle = FontStyle
+        };
+    }
+}
