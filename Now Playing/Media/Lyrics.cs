5ce8213d08432ecd02792ce5975d389e79419e58
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-06-19 12:50:18 -0700

    Move LRCLIB API wrappers to Media namespace

---
+++ Now Playing/Media/Lyrics.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+
+namespace Now_Playing.Media;
+
+public class Lyrics : IParsable<Lyrics>
+{
+    private const int TimestampLength = 9;
+
+    public LyricLine[] Lines => lines;
+    private LyricLine[] lines;
+
+    public string GetTextForTimestamp(TimeSpan timestamp) => lines[GetIndexForTimestamp(timestamp)].Text;
+
+    public int GetIndexForTimestamp(TimeSpan timestamp)
+    {
+        for (int i = 1; i < lines.Length; ++i)
+        {
+            if (timestamp < lines[i].Timestamp)
+                return i - 1;
+        }
+
+        return lines.Length - 1;
+    }
+
+    public static Lyrics Parse(string syncedLyrics) => Parse(syncedLyrics, null);
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static Lyrics Parse(string syncedLyrics, IFormatProvider format)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(syncedLyrics, nameof(syncedLyrics));
+
+        if (!TryParse(syncedLyrics, format, out Lyrics lyrics))
+            throw new FormatException();
+
+        return lyrics;
+    }
+
+    public static bool TryParse(string syncedLyrics, out Lyrics lyrics) => TryParse(syncedLyrics, null, out lyrics);
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static bool TryParse(string syncedLyrics, IFormatProvider format, out Lyrics lyrics)
+    {
+        string[] lines = syncedLyrics.Split('\n');
+        Lyrics result = new() { lines = new LyricLine[lines.Length + 1] };
+
+        // Empty entry for start of the song
+        result.lines[0] = new(TimeSpan.Zero, string.Empty);
+
+        int lineIndex = 1;
+        foreach (string line in lines)
+        {
+            string timestampString = line[1..TimestampLength];
+
+            string text = string.Empty;
+            if (line.Length > TimestampLength + 1)
+                text = line[(TimestampLength + 2)..];
+
+            if (!TimeSpan.TryParse("00:" + timestampString, out TimeSpan timestamp))
+            {
+                lyrics = null;
+                return false;
+            }
+
+            // Round the timestamp down to avoid text showing up too late
+            // (it can still show up too early)
+            timestamp = TimeSpan.FromSeconds(Math.Floor(timestamp.TotalSeconds));
+
+            result.lines[lineIndex] = new(timestamp, text);
+            ++lineIndex;
+        }
+
+        lyrics = result;
+        return true;
+    }
+}
+
+public class LyricLine(TimeSpan timestamp, string text)
+{
+    public TimeSpan Timestamp { get; } = timestamp;
+    public string Text { get; } = text;
+}
---
+++ Now Playing/Media/Song.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Now_Playing.Media;
+
+public class Song
+{
+    #region JSON Properties
+
+    [JsonPropertyName("id")]
+    public string ID { get; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; }
+
+    [JsonPropertyName("trackName")]
+    public string TrackName { get; }
+
+    [JsonPropertyName("artistName")]
+    public string ArtistName { get; }
+
+    [JsonPropertyName("albumName")]
+    public string AlbumName { get; }
+
+    [JsonPropertyName("duration")]
+    public string Duration { get; }
+
+    [JsonPropertyName("instrumental")]
+    public string Instrumental { get; }
+
+    [JsonPropertyName("plainLyrics")]
+    public string PlainLyrics { get; }
+
+    [JsonPropertyName("syncedLyrics")]
+    public string SyncedLyrics { get; }
+
+    #endregion
+
+    public Lyrics Lyrics { get; }
+
+    public Song()
+    {
+         if (SyncedLyrics is not null)
+            Lyrics = Lyrics.Parse(SyncedLyrics);
+    }
+
+    public static async Task<Song> FromSongID(string songID)
+    {
+        HttpResponseMessage response = await MakeAPIRequest($"/api/get/{songID}");
+
+        return response.IsSuccessStatusCode ? JsonSerializer.Deserialize<Song>(await response.Content.ReadAsStringAsync()) :
+            throw new HttpListenerException((int)response.StatusCode, response.ReasonPhrase);
+    }
+
+    public static async Task<Song> FromTrackSignature(string trackSignature, bool getCached)
+    {
+        HttpResponseMessage response = await MakeAPIRequest($"/{(getCached ? "get-cached?" : "get?")}{trackSignature}");
+
+        return response.IsSuccessStatusCode ? JsonSerializer.Deserialize<Song>(await response.Content.ReadAsStringAsync()) :
+            throw new HttpListenerException((int)response.StatusCode, response.ReasonPhrase);
+    }
+
+    public static async Task<Song[]> FromSearch(string trackName, string artistName = null)
+    {
+        HttpResponseMessage response = await MakeAPIRequest(
+            $"/search?track_name={WebUtility.UrlEncode(trackName).ToLower()}{(artistName is null ? string.Empty : $"&artist_name={WebUtility.UrlEncode(artistName).ToLower()}")}");
+
+        return response.IsSuccessStatusCode ? JsonSerializer.Deserialize<Song[]>(await response.Content.ReadAsStringAsync()) :
+            throw new HttpListenerException((int)response.StatusCode, response.ReasonPhrase);
+    }
+
+    private const string LRCLIB = "https://lrclib.net/";
+
+    private static Task<HttpResponseMessage> MakeAPIRequest(string path)
+    {
+        HttpClient client = new()
+        {
+            Timeout = TimeSpan.FromMinutes(1),
+            BaseAddress = new(LRCLIBAPI)
+        };
+
+        return client.GetAsync($"/api/{path}");
+    }
+}
